24d0259faa8528deec82397da311b9235def21cf
Meiying Hu <meiyinghu18@gmail.com>
2026-01-21 21:07:30 +0100

    securityConfig

--- src/main/java/se/jensen/meiying/socialapp/security/SecurityConfig.java
+++ src/main/java/se/jensen/meiying/socialapp/security/SecurityConfig.java
@@ -71,6 +71,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/",
                                 "/users/login",
                                 "/users/",
                                 "/users",
